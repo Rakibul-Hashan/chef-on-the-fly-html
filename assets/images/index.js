82cea7655cde20c4d404ce6737bda1b9ef8e496d
Rakibul Hashan pHero <rakibulhashan1603@gmail.com>
2024-03-24 05:48:51 +0600

    Navbar css - Desktop

---
+++ assets/images/index.js
@@ -0,0 +1,58 @@
+const IMAGES = {
+  logo: require("./Logo.png"),
+  home: {
+    heroImg: require("./hero.png"),
+    bookCover: require("./book-cover-final.png"),
+    second1: require("./book-meeting 1.png"),
+    second2: require("./tell-us.png"),
+    second3: require("./we-find.png"),
+    whoweare: require("./who-we-are.png"),
+    clientlogo1: require("./Logo 01.png"),
+    clientlogo2: require("./Logo 02.png"),
+    clientlogo3: require("./Logo 03.png"),
+    clientlogo4: require("./Logo 04.png"),
+    clientlogo5: require("./Logo 05.png"),
+    clientlogo6: require("./Logo 06.png"),
+  },
+  services: {
+    contract: require("./contract.png"),
+    placement: require("./Permanent Placement.png"),
+    privateChef: require("./Private Chef.png"),
+  },
+  sucessStories: {
+    contract0: require("../images/contracts/contract-0.jpg"),
+    contract1: require("../images/contracts/contract-1.jpg"),
+    contract2: require("../images/contracts/contract-2.jpg"),
+    contract3: require("../images/contracts/contract-3.jpg"),
+    contract4: require("../images/contracts/contract-4.jpg"),
+    contract5: require("../images/contracts/contract-5.jpg"),
+    contract6: require("../images/contracts/contract-6.jpg"),
+    contract7: require("../images/contracts/contract-7.jpg"),
+    contract8: require("../images/contracts/contract-8.jpg"),
+    contract9: require("../images/contracts/contract-9.jpg"),
+    contract10: require("../images/contracts/contract-10.jpg"),
+    contract11: require("../images/contracts/contract-11.jpg"),
+    contract12: require("../images/contracts/contract-12.jpg"),
+    contract13: require("../images/contracts/contract-13.jpg"),
+    contract14: require("../images/contracts/contract-14.jpg"),
+    contract15: require("../images/contracts/contract-15.jpg"),
+    contract16: require("../images/contracts/contract-16.jpg"),
+    contract17: require("../images/contracts/contract-17.jpg"),
+  },
+  faq:{
+    arrow: require("./Arrow - Down 2.png")
+  },
+  aboutUs:{
+    hero: require("./about-us/hero.png"),
+    ourvision: require("./about-us/ourvision.png"),
+    ceo: require("./about-us/ceo.png"),
+    icon1: require("./about-us/icon1.png"),
+    icon2: require("./about-us/icon2.png"),
+    partner1: require("./about-us/partner1.png"),
+    partner2: require("./about-us/partner2.png"),
+    partner3: require("./about-us/partner3.png"),
+    
+    
+  }
+};
+export default IMAGES;
